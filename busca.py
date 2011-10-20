8f59fc74ac4bf8b77816342ba26af0a5b1003714
Francisco Jose Navarro Garcia <fran@fjnavarro.es>
2011-10-20 09:57:43 +0200

    version 1

---
+++ busca.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import commands
+import sys
+"Busca in python"
+numberParams=len(sys.argv)
+if(numberParams>1):
+    findTxt='find . -type f'
+    if(numberParams>2):
+        findTxt+='|egrep "'
+        for i in range(len(sys.argv)):
+            if(i>1):
+                findTxt+=sys.argv[i]
+                if(i+1<numberParams):
+                    findTxt+='|'
+        findTxt+='"'
+    find=commands.getoutput(findTxt)
+    for value in find.split('n'):
+        value2=commands.getoutput('grep -nHi "'+sys.argv[1]+'" '+value)
+        if value2!='':
+            print value2
+else:
+    print "Please, busca.py 'searchString' [typeFile typeFile ...]"
