1e27f3fc7923509fc2e42e4bef56a635dcada7b7
Francisco Jose Navarro Garcia <fran@fjnavarro.es>
2011-11-14 17:48:51 +0100

    bug no results

--- busca.py
+++ busca.py
@@ -9,14 +9,17 @@ if(numberParams>1):
         findTxt+='|egrep "'
         for i in range(len(sys.argv)):
             if(i>1):
-                findTxt+=sys.argv[i]
+                findTxt+='\.'+sys.argv[i]
                 if(i+1<numberParams):
                     findTxt+='|'
         findTxt+='"'
     find=commands.getoutput(findTxt)
-    for value in find.split('n'):
-        value2=commands.getoutput('grep -nHi "'+sys.argv[1]+'" '+value)
-        if value2!='':
-            print value2
+    if(len(find)>1):
+        for value in find.split('n'):
+            value2=commands.getoutput('grep -nHi "'+sys.argv[1]+'" '+value)
+            if value2!='':
+                print value2
+    else:
+        print "no results"
 else:
     print "Please, busca.py 'searchString' [typeFile typeFile ...]"
